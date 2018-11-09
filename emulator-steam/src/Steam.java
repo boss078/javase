b9bbcb0ca4d6ad95138c1f890e9aaad29fe87a39
boss078 <36530564+boss078@users.noreply.github.com>
2018-11-09 22:53:13 +0300

    Delete Steam.java

--- emulator-steam/src/Steam.java
+++
@@ -1,7 +0,0 @@
-public class Steam {
-    private double totalCash;
-    private Game totalGames[] = new Game[10];
-    public void sell(Game aGame, Player aPlayer) throws SteamSellException {
-        for
-    }
-}
